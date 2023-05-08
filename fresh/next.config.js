8316e15f8738a1d36255e0aacf423f7799a62f8b
Jonguk Lee <looke2930@gmail.com>
2023-05-09 01:57:35 +0900

    1강 : 세팅하기

---
+++ fresh/next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
+
+// module.exports = nextConfig
+// next.config.js 파일
+
+module.exports = {
+  experimental: {
+    appDir: true
+  }
+}
