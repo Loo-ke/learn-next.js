7cec8a05e815feb5124afb7d5c14f6d8db649e81
Jonguk Lee <looke2930@gmail.com>
2023-06-01 20:03:09 +0900

    test

--- fresh/app/page.js
+++ fresh/app/page.js
@@ -7,6 +7,7 @@ export default function Home() {
     <div>
       <h4 className="title" style={{color:"blue", fontSize:30}}>종욱후레시</h4>
       <p className='title-sub'>by Luke {name}</p>
+      <div>test</div>
     </div>
   )
 }
