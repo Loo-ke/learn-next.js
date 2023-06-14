da808a125bcdb15b89da27da608b6b9ab0dd227c
Jonguk Lee <looke2930@gmail.com>
2023-06-14 13:56:29 +0900

    2강 스타일 및 변수 선언

--- fresh/app/page.js
+++ fresh/app/page.js
@@ -3,10 +3,12 @@ import styles from './page.module.css'
 
 export default function Home() {
   let name = "Lee"
+  let link = "http://google.com"
   return (
     <div>
       <h4 className="title" style={{color:"blue", fontSize:30}}>종욱후레시</h4>
       <p className='title-sub'>by Luke {name}</p>
+      <a href={link}>링크</a>
       <div>test</div>
     </div>
   )
