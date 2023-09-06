dceab6f0da65cf2d9c3e57f58c3de69998305d16
Jonguk Lee <looke2930@gmail.com>
2023-09-06 21:46:22 +0900

    write 페이지 로그인 시 이용 가능하게 만듬

--- forum/app/write/page.js
+++ forum/app/write/page.js
@@ -5,15 +5,19 @@ export default async function Write() {
   let session = await getServerSession(authOptions)
 
   if(session){
-    
+    return (
+      <div className="p-20">
+        <form action="/api/post" method="POST">
+          <input name="title" placeholder="글제목"/>
+          <input name="content" placeholder="글내용"/>
+          <button type="submit">전송</button>
+        </form>
+      </div>
+    )
+  }else{
+    return(
+      <h2>로그인 후 이용해주세요.</h2>
+    )
   }
-  return (
-    <div className="p-20">
-      <form action="/api/post" method="POST">
-        <input name="title" placeholder="글제목"/>
-        <input name="content" placeholder="글내용"/>
-        <button type="submit">전송</button>
-      </form>
-    </div>
-  )
+  
 } 
